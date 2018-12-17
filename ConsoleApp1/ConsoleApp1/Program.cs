fe035e180be00858f49867c836eaf7288068587d
DESKTOP-IA8B0AR\developer <dmitriymudrak@gmailc.om>
2018-12-17 11:15:48 +0200

    -finish factories

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,73 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var modernStyleFactory = new ModernStyleFactory();
+            var resultChair = modernStyleFactory.CreateChair();
+            var resultTable = modernStyleFactory.CreateTable();
         }
         
     }
+    public abstract class Chair
+    {
+
+    }
+
+    public class ModernStyleChair : Chair
+    {
+
+    }
+
+    public class VictorianStyleChair : Chair
+    {
+
+    }
+
+    public abstract class Table
+    {
+
+    }
+
+    public class ModernStyleTable : Table
+    {
+
+    }
+
+    public class VictorianStyleTable : Table
+    {
+
+    }
+    
+    public abstract class FurnitureFactory
+    {
+        public abstract Chair CreateChair();
+        public abstract Table CreateTable();
+    }
+
+    public class ModernStyleFactory : FurnitureFactory
+    {
+        public override Chair CreateChair()
+        {
+            return new ModernStyleChair();
+        }
+
+        public override Table CreateTable()
+        {
+            return new ModernStyleTable();
+        }
+    }
+
+    public class VictorianStyleFactory : FurnitureFactory
+    {
+        public override Chair CreateChair()
+        {
+            return new VictorianStyleChair();
+        }
+
+        public override Table CreateTable()
+        {
+            return new VictorianStyleTable();
+        }
+    }
+
+
 }
